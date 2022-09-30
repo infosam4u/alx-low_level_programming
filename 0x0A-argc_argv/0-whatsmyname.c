9d432d216ff187912d83b27ddfc94cea4440a364
ADEDIPE SAMUEL OTON <33044802+infosam4u@users.noreply.github.com>
2022-09-30 20:44:53 +0100

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,7 +4,7 @@
  * main - Prints the program name, followed by a new line.
  * @argc: The number of arguments supplied to the program.
  * @argv: An array of pointers to the arguments.
- * Author: Ismail Mohamed - Addiss Ababa Intranet.
+ * Author: Infosam4u
  * Return: Always o.
  */
 int main(int __attribute__((__unused__)) argc, char *argv[])
