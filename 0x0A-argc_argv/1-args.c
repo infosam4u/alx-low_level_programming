dfa61e0ee1d55192e2311f4d3aa872ffd1cde65e
ADEDIPE SAMUEL OTON <33044802+infosam4u@users.noreply.github.com>
2022-09-30 20:30:56 +0100

    Update 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -4,7 +4,7 @@
  * main - Prints the number of arguments passed to it.
  * @argc: The number of arguments supplied to the program.
  * @argv: An array of pointers to the arguments.
- * Author: Ismail Mohamed  - ALX - SE - COHORT_6
+ * Author: Infosam4u
  * Return: Always 0.
  */
 int main(int argc, char __attribute__((__unused__)) *argv[])
